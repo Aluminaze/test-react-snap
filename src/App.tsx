2988808947de8ace6f435fd0c82ecab4dd58ac2b
Khegay Artem <aluminaze@gmail.com>
2023-08-09 12:00:17 +0600

    init app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <BrowserRouter>
+      <header>
+        <ul
+          style={{
+            display: "inline-flex",
+            gap: 20,
+            justifyContent: "center",
+            width: "100%",
+          }}
         >
-          Learn React
-        </a>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/blog">Blog</Link>
+          </li>
+        </ul>
       </header>
-    </div>
+
+      <Switch>
+        <Route path={"/"} exact>
+          <h1>Main</h1>
+        </Route>
+        <Route path={"/about"} exact>
+          <h1>About</h1>
+        </Route>
+        <Route path={"/blog"} exact>
+          <h1>Blog</h1>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
